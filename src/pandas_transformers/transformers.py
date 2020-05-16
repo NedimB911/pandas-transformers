00a9e31f59edb88595667ba0fccf0caffa9d0791
NedimB911 <nedim_@live.nl>
2020-05-16 14:21:41 +0200

    added pylint to pre-commit

--- src/pandas_transformers/transformers.py
+++ src/pandas_transformers/transformers.py
@@ -1,5 +1,9 @@
-print("test")
+# pylint: disable=missing-module-docstring
 
 
 def test():
+    """
+    Test docstring
+    """
     print("hi")
+    print("holaa")
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,16 +1,26 @@
+# pylint: disable=missing-module-docstring
 from pandas_transformers.transformers import test
 
 
 class TestExample:
+    """
+    Test class
+    """
+
     def test_example(self):
+        """
+        Test example
+        """
         print("example test")
 
 
-class TestImports:
-    def test_pandas(self):
-        pass
-
-
 class TestTest:
+    """
+    Test class
+    """
+
     def test_test(self):
+        """
+        Testing test
+        """
         test()
