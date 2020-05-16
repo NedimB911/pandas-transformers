7e395c29c7114f6ff310533d689425343cb2e182
NedimB911 <nedim_@live.nl>
2020-05-16 13:38:09 +0200

    added tests

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 from setuptools import find_packages, setup
 
+with open("requirements-dev.txt") as f:
+    test_requirements = f.read().splitlines()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+extra_requirements = {"dev": test_requirements}
+
+setup_requirements = ["pytest-runner"]
+
 setup(
-    name='pandas-transformers',
+    name="pandas-transformers",
     packages=find_packages("src"),
-    package_dir={"": "src"}, 
-    version='0.1.0',
-    description='Pandas equivalents of (e.g.) the beloved scikit-learn transformers.',
-    author='Nedim Bayrakdar',
-    license='MIT',
+    package_dir={"": "src"},
+    version="0.1.0",
+    description="Pandas equivalents of (e.g.) the beloved scikit-learn transformers.",
+    author="Nedim Bayrakdar",
+    license="MIT",
+    test_suite="tests",
+    install_requires=requirements,
+    tests_require=test_requirements,
+    extras_require=extra_requirements,
+    setup_requires=setup_requirements,
 )
---
+++ tests/test_example.py
@@ -0,0 +1,16 @@
+from pandas_transformers.transformers import test
+
+
+class TestExample:
+    def test_example(self):
+        print("example test")
+
+
+class TestImports:
+    def test_pandas(self):
+        pass
+
+
+class TestTest:
+    def test_test(self):
+        test()
